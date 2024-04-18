9af0c94ecf0c6953d80061a0c7524cea398e6824
Keon <lune0410k@naver.com>
2024-04-18 20:04:58 +0900

    feat: Response class added Response class added.

---
+++ src/main/java/com/blossom/lineup/base/Response.java
@@ -0,0 +1,53 @@
+package com.blossom.lineup.base;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(content = JsonInclude.Include.NON_NULL)
+public class Response<T> {
+
+	private String code;
+	private String message;
+	private T data;
+
+	public Response(String code) {
+		this.code = code;
+	}
+
+	public Response(String code, T data) {
+		this.code = code;
+		this.data = data;
+	}
+
+	public Response(String code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+	public static Response<Void> ok(){
+		return new Response<>(Code.OK.getCode());
+	}
+
+	public static <T> Response<T> ok(T data) {
+		return new Response<>(Code.OK.getCode(), data);
+	}
+
+	public static <E> Response<Void> fail(String code) {
+		return new Response<>(code);
+	}
+
+	public static <E> Response<Void> fail(String code, String message) {
+		return new Response<>(code, message);
+	}
+
+}
