c6302552dba87f02713aba2a30ce3a0ed2f86d58
HJunng <guswjd1gh@gmail.com>
2024-04-19 02:57:29 +0900

    feature : unique 값에 넣기 위한 LocalDate createDate 필드 생성

--- src/main/java/com/blossom/lineup/Waiting/entity/Waiting.java
+++ src/main/java/com/blossom/lineup/Waiting/entity/Waiting.java
@@ -1,20 +1,20 @@
 package com.blossom.lineup.Waiting.entity;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.*;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.SQLRestriction;
-
 import com.blossom.lineup.Member.entity.Customer;
 import com.blossom.lineup.Organization.entity.Organization;
 import com.blossom.lineup.Waiting.util.EntranceStatus;
 import com.blossom.lineup.base.BaseEntity;
-
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -23,7 +23,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(uniqueConstraints = {
 		// (주점, 대기번호, 대기날짜)가 unique
-		@UniqueConstraint(columnNames = {"organization_id", "waiting_number", "date(createdAt)"})
+		@UniqueConstraint(columnNames = {"organization_id", "waiting_number", "createdDate"})
 })
 public class Waiting extends BaseEntity {
 
@@ -32,20 +32,28 @@ public class Waiting extends BaseEntity {
 	@Column(name = "waiting_id")
 	private Long id;
 
-	private Integer headCount; 				// 인원수
-	private LocalDateTime entranceTime;     // 입장 시간
-	private Integer tableNumber;            // 배정받은 테이블 번호
+	@NotNull
 	private Integer waitingNumber;			// 대기 번호
+
+	@NotNull
 	@Enumerated(value = EnumType.STRING)
 	private EntranceStatus entranceStatus;	// 입장 상태
 
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "organization_id")
 	private Organization organization;		// 주점
 
+	@NotNull
 	@OneToOne
 	@JoinColumn(name = "customer_id")
 	private Customer customer;				// 대기자
+
+	private Integer headCount; 				// 인원수
+	private LocalDateTime entranceTime;     // 입장 시간
+	private Integer tableNumber;            // 배정받은 테이블 번호
+
+	private LocalDate createdDate;
 
 	@Builder
 	public Waiting(Integer headCount, LocalDateTime entranceTime, Integer tableNumber, Integer waitingNumber,
@@ -57,5 +65,6 @@ public class Waiting extends BaseEntity {
 		this.organization = organization;
 		this.customer = customer;
 		this.entranceStatus = entranceStatus;
+		this.createdDate = getCreatedAt().toLocalDate();
 	}
 }
