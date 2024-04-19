2d5b4c558bc90070f1a5691b05d2d7cbe44c7748
HJunng <guswjd1gh@gmail.com>
2024-04-20 04:26:09 +0900

    fix : JpaRepository를 사용해서 검색할 때, 한 필드가 정해진 값인 건 Query로 조회하는게 더 정확함

--- src/main/java/com/blossom/lineup/Waiting/WaitingRepository.java
+++ src/main/java/com/blossom/lineup/Waiting/WaitingRepository.java
@@ -3,6 +3,8 @@ package com.blossom.lineup.Waiting;
 import com.blossom.lineup.Organization.entity.Organization;
 import com.blossom.lineup.Waiting.entity.Waiting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,10 +12,12 @@ import java.util.List;
 @Repository
 public interface WaitingRepository extends JpaRepository<Waiting, Long> {
 
-    // 나를 포함한 대기 명단
+    // 나 이전의 대기 명단
     // (organization에 대기하고 있고(waiting) 내 id보다 작은 id를 가지고 있는 waiting 명단을 id 오름차순으로 정렬)
-    List<Waiting> findByOrganizationAndEntranceStatusWaitingAndIdLessThanOrderById(Organization organization, Long id);
+    @Query("SELECT w FROM Waiting w WHERE w.organization =:organization AND w.entranceStatus = 'WAITING' AND w.id = :id ORDER BY w.id")
+    List<Waiting> findBeforeMyWaiting(@Param("organization") Organization organization, @Param("id") Long id);
 
     // 이용하고 있는 테이블의 모든 Waiting 정보 (입장시각순으로 정렬)
-    List<Waiting> findByOrganizationAndEntranceStatusCompleteOrderByEntranceTime(Organization organization);
+    @Query("SELECT w FROM Waiting w WHERE w.organization =:organization AND w.entranceStatus = 'COMPLETE' ORDER BY w.entranceTime")
+    List<Waiting> findUsingTables(@Param("organization") Organization organization);
 }
