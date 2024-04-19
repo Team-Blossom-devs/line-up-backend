91deb1d188426c1d6ce9c5b29f050b5dd1238b5b
HJunng <guswjd1gh@gmail.com>
2024-04-20 03:36:14 +0900

    feature : 대기명단 조회, 테이블을 이용하고 있는 Waiting 정보 조회 구현 - List타입으로 봔환하는 걸로 변경

--- src/main/java/com/blossom/lineup/Waiting/WaitingRepository.java
+++ src/main/java/com/blossom/lineup/Waiting/WaitingRepository.java
@@ -3,14 +3,17 @@ package com.blossom.lineup.Waiting;
 import com.blossom.lineup.Organization.entity.Organization;
 import com.blossom.lineup.Waiting.entity.Waiting;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WaitingRepository extends JpaRepository<Waiting, Long> {
 
-    // 내 앞의 대기 인원
-    @Query("SELECT COUNT(w) FROM Waiting w WHERE w.organization = :organization AND w.waitingNumber <= :waitingNumber AND w.entranceStatus = 'WAITING'")
-    int countWaitingWithLowerNumber(@Param("organization") Organization organization, @Param("waitingNumber") Integer waitingNumber);
+    // 나를 포함한 대기 명단
+    // (organization에 대기하고 있고(waiting) 내 id보다 작은 id를 가지고 있는 waiting 명단을 id 오름차순으로 정렬)
+    List<Waiting> findByOrganizationAndEntranceStatusWaitingAndIdLessThanOrderById(Organization organization, Long id);
+
+    // 이용하고 있는 테이블의 모든 Waiting 정보 (입장시각순으로 정렬)
+    List<Waiting> findByOrganizationAndEntranceStatusCompleteOrderByEntranceTime(Organization organization);
 }
