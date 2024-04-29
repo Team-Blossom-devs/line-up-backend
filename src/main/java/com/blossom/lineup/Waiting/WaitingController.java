5df33d766c04e881b5e2cbd15049b4b9bfc909c5
HJunng <guswjd1gh@gmail.com>
2024-04-29 21:45:23 +0900

    feature : 대기 create, delete 컨트롤러 구현 및 테스트 완료

--- src/main/java/com/blossom/lineup/Waiting/WaitingController.java
+++ src/main/java/com/blossom/lineup/Waiting/WaitingController.java
@@ -1,13 +1,11 @@
 package com.blossom.lineup.Waiting;
 
+import com.blossom.lineup.Waiting.entity.request.WaitingRequest;
 import com.blossom.lineup.Waiting.entity.response.CheckWaitingStatus;
 import com.blossom.lineup.base.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -17,9 +15,25 @@ public class WaitingController {
 
     private final WaitingService waitingService;
 
+    @PostMapping("/waiting")
+    public Response<Void> create(@RequestBody WaitingRequest request){
+        log.debug("대기 생성");
+
+        waitingService.create(request);
+        return Response.ok();
+    }
+
+    @DeleteMapping("/waiting/{waitingId}")
+    public Response<Void> delete(@PathVariable("waitingId") long waitingId){
+        log.debug("대기 삭제");
+
+        waitingService.delete(waitingId);
+        return Response.ok();
+    }
+
     // todo : waitingId없이 (대기 걸어두지 않았을 때)의 대기 현황 조회 추가하기.
     @GetMapping("/waiting-status/{waitingId}")
-    public Response<CheckWaitingStatus> getCurrentWaitingStatus(@PathVariable("waitingId") Long waitingId){
+    public Response<CheckWaitingStatus> getCurrentWaitingStatus(@PathVariable("waitingId") long waitingId){
         log.debug("waitingId : "+waitingId);
 
         return Response.ok(waitingService.myCurrentWaiting(waitingId));
