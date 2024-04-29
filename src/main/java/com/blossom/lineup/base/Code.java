d472e724fc6e5fea5c71495b64da731a3a63bc5b
HJunng <guswjd1gh@gmail.com>
2024-04-29 21:43:37 +0900

    feature : 에러 코드 추가

--- src/main/java/com/blossom/lineup/base/Code.java
+++ src/main/java/com/blossom/lineup/base/Code.java
@@ -10,8 +10,15 @@ public enum Code {
 	// OK
 	OK(HttpStatus.OK, "COM-000", "ok"),
 
+	// CST : Customer entity error
+	CUSTOMER_NOT_FOUND(HttpStatus.BAD_REQUEST, "CST-001", "회원을 찾을 수 없습니다."),
+
+	// ORG : Organization entity error
+	ORGANIZATION_NOT_FOUND(HttpStatus.BAD_REQUEST, "ORG-001", "주점을 찾을 수 없습니다."),
+
 	// WAT : Waiting entity error
 	WAITING_NOT_FOUND(HttpStatus.BAD_REQUEST, "WAT-001", "대기 기록을 찾을 수 없습니다."),
+	WAITING_DUPLICATE(HttpStatus.BAD_REQUEST, "WAT-002", "대기는 1곳만 가능합니다."),
 	WAITING_NUMBER_IS_NULL(HttpStatus.BAD_REQUEST, "WAT-101", "대기 번호가 null 입니다."),
 
 	ENTRANCE_TIME_IS_NULL(HttpStatus.BAD_REQUEST, "WAT-501", "입장시간이 null 인 테이블이 존재합니다."),
