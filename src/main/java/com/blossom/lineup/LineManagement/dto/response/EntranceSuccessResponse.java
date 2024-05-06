9c60a1a4a361e3317ba7538f22e764e2615c8cd9
Keon <lune0410k@naver.com>
2024-05-06 16:07:30 +0900

    feat: entrance success reponse dto added

---
+++ src/main/java/com/blossom/lineup/LineManagement/dto/response/EntranceSuccessResponse.java
@@ -0,0 +1,14 @@
+package com.blossom.lineup.LineManagement.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EntranceSuccessResponse {
+
+	private String redirectUrl;
+
+}
