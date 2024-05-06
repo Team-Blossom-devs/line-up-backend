0066ab7a196c13d62023e60bf40601926737df21
je326 <mya326@naver.com>
2024-05-06 19:02:02 +0900

    feature: 요구사항에 맞게 엔티티 수정 및 초기 데이터 추가

--- src/main/java/com/blossom/lineup/Member/entity/Member.java
+++ src/main/java/com/blossom/lineup/Member/entity/Member.java
@@ -11,25 +11,31 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
 
 @Getter
 @MappedSuperclass
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public abstract class Member extends BaseEntity implements BaseMember{
+public abstract class Member extends BaseEntity implements BaseMember {
 
 	private String userName;
 	private String phoneNumber;
-	private Long socialId;
 	@Enumerated(value = EnumType.STRING)
 	private Role role;
-	private String email;
+	private String uuid;
+	private String refreshToken; //JWT 사용하여 로그인 시 발행된 RefreshToken을 저장한다.
 
-	protected Member(String userName, String phoneNumber, Long socialId, Role role, String email) {
+	protected Member(String userName, String phoneNumber,Role role, String refreshToken) {
 		this.userName = userName;
 		this.phoneNumber = phoneNumber;
-		this.socialId = socialId;
 		this.role = role;
-		this.email = email;
+		this.uuid = UUID.randomUUID().toString();
+		this.refreshToken = refreshToken;
 	}
 
+	public void updateRefreshToken(String updateRefreshToken) {
+		this.refreshToken = updateRefreshToken;
+	}
 }
