6af66ccc9fd90aeb08f3d9f74ce57c9f064071d0
Keon <lune0410k@naver.com>
2024-05-06 16:56:09 +0900

    refactor: refactor for js script seperate from html

---
+++ src/main/resources/static/js/entrance.js
@@ -0,0 +1,34 @@
+const sendData = async (waitingId, tableNumber, tableCount) => {
+    if (tableNumber === null || tableCount === null){
+        console.log(tableNumber);
+        return;
+    }
+
+    await axios.patch("http://localhost:8080/api/manager/complete", {
+        id: waitingId,
+        tableNumber: tableNumber,
+        tableCount: tableCount
+    })
+        .then((response) => {
+            console.log(response)
+            if (response.data.code === 'COM-000'){
+                window.alert('입장 처리 완료');
+                window.location.href = response.data.data.redirectUrl;
+            } else {
+                throw new Error();
+            }
+        })
+        .catch((error) => {
+            console.error('Error: ', error);
+            window.alert('입장 처리에 실패하였습니다. 운영자에게 문의하세요.');
+        })
+}
+
+function getInputValue(idName) {
+    const inputValue = document.getElementById(idName).value;
+    if (inputValue === '') {
+        window.alert('입력 값이 필요합니다!')
+        return null;
+    }
+    return parseInt(inputValue);
+}
