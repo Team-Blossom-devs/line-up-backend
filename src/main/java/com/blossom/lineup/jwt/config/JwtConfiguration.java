909f28c7618114252f712181ff126b52979e9de7
je326 <mya326@naver.com>
2024-05-07 03:22:43 +0900

    refactor: properties 바인딩 방식 변경

--- src/main/java/com/blossom/lineup/jwt/config/JwtConfiguration.java
+++ src/main/java/com/blossom/lineup/jwt/config/JwtConfiguration.java
@@ -1,69 +1,26 @@
 package com.blossom.lineup.jwt.config;
 
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import javax.crypto.SecretKey;
-import java.util.concurrent.TimeUnit;
-
+@Slf4j
+@Data
 @Configuration
+@ConfigurationProperties(prefix = "jwt")
 public class JwtConfiguration {
 
-    @Value("${jwt.secret}")
-    private String secretKey;
+    private String secret;
 
-    @Value("${jwt.access.expiration}")
-    private Long accessTokenExpire;
+    private Long accessExpiration;
 
-    @Value("${jwt.refresh.expiration}")
-    private Long refreshTokenExpire;
+    private Long refreshExpiration;
 
-    @Value("${jwt.access.header}")
     private String accessHeader;
 
-    @Value("${jwt.refresh.set_cookie}")
     private String refreshSetCookie;
 
-    @Value("${jwt.cookie.expiration}")
-    private Long cookieExpire;
+    private Long cookieExpiration;
 
-    @Bean
-    public SecretKey secretKey() {
-        byte[] keyBytes = Decoders.BASE64URL.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
-
-    @Bean
-    @Qualifier("accessTokenExpire")
-    public Long AccessTokenExpire() {
-        return TimeUnit.MINUTES.toMillis(accessTokenExpire);
-    }
-
-    @Bean
-    @Qualifier("refreshTokenExpire")
-    public Long refreshTokenExpire() {
-        return TimeUnit.MINUTES.toMillis(refreshTokenExpire);
-    }
-
-    @Bean
-    @Qualifier("accessHeader")
-    public String AccessHeader() {
-        return accessHeader;
-    }
-
-    @Bean
-    @Qualifier("refreshSetCookie")
-    public String refreshSetCookie() {
-        return refreshSetCookie;
-    }
-
-    @Bean
-    @Qualifier("cookieExpire")
-    public Long cookieExpire() {
-        return TimeUnit.MINUTES.toMillis(cookieExpire);
-    }
 }
