5097dc198aca0f428f501c72cb66b274fcdad4f1
HJunng <guswjd1gh@gmail.com>
2024-05-10 01:03:50 +0900

    feature : 현재 대기 상태에 따라 응답값 다르게 return.

--- src/main/java/com/blossom/lineup/Waiting/service/WaitingService.java
+++ src/main/java/com/blossom/lineup/Waiting/service/WaitingService.java
@@ -1,13 +1,20 @@
 package com.blossom.lineup.Waiting.service;
 
+import com.blossom.lineup.Waiting.entity.Waiting;
 import com.blossom.lineup.Waiting.entity.request.WaitingRequest;
-import com.blossom.lineup.Waiting.entity.response.CheckWaitingStatus;
+import com.blossom.lineup.Waiting.entity.response.PendingResponse;
+import com.blossom.lineup.Waiting.entity.response.WaitingResponse;
+import com.blossom.lineup.base.Response;
 
 public interface WaitingService {
 
     void create(WaitingRequest request); // 대기 생성
 
-    CheckWaitingStatus myCurrentWaiting(Long waitingId); // 대기현황 조회
+    Response<?> getWaitingStatus(Long organizationId);
+
+    WaitingResponse myCurrentWaiting(Waiting waiting, Long organizationId); // 대기현황 조회
+
+    PendingResponse getPendingStatus(Waiting waiting); // 입장중 상태 조회
 
     void delete(Long waitingId); // 대기
 }
