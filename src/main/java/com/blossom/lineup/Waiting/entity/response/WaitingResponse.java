80864a51b3a4458b96efa43dd676030dfb826eae
HJunng <guswjd1gh@gmail.com>
2024-05-10 02:37:09 +0900

    fix : 함수 제거, null이 아닌 필드만 반환

--- src/main/java/com/blossom/lineup/Waiting/entity/response/WaitingResponse.java
+++ src/main/java/com/blossom/lineup/Waiting/entity/response/WaitingResponse.java
@@ -1,17 +1,15 @@
 package com.blossom.lineup.Waiting.entity.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class WaitingResponse { // 대기 현황
     private String waitingStatus; // 대기 상태
     private int currentWaitingNumber; // 현재 내 순서
     private int expectWaitingTime;  // 예상 대기 시간
-    private int headCount;            // 인원 수
-
-    public static WaitingResponse of(String waitingStatus, int beforeMeCnt, int expectWaitingTime, int headCount){
-        return new WaitingResponse(waitingStatus, beforeMeCnt, expectWaitingTime, headCount);
-    }
+    private Integer headCount;            // 인원 수
 }
