af40d5894e4b22f7bf3322106bfabe0a6055404d
HJunng <guswjd1gh@gmail.com>
2024-05-11 03:40:18 +0900

    feature : 주점 상세 정보 조회하는 api 구현

--- src/main/java/com/blossom/lineup/Organization/service/OrganizationService.java
+++ src/main/java/com/blossom/lineup/Organization/service/OrganizationService.java
@@ -1,7 +1,10 @@
 package com.blossom.lineup.Organization.service;
 
+import com.blossom.lineup.Organization.entity.response.OrganizationDto;
 import com.blossom.lineup.Organization.entity.response.OrganizationSearchList;
 
 public interface OrganizationService {
+
+    OrganizationDto getOrganization(long organizationId);
     OrganizationSearchList searchOrganizations(String searchTerm, int pageNum);
 }
--- src/main/java/com/blossom/lineup/Organization/service/OrganizationServiceImpl.java
+++ src/main/java/com/blossom/lineup/Organization/service/OrganizationServiceImpl.java
@@ -1,8 +1,11 @@
 package com.blossom.lineup.Organization.service;
 
 import com.blossom.lineup.Organization.entity.Organization;
+import com.blossom.lineup.Organization.entity.response.OrganizationDto;
 import com.blossom.lineup.Organization.entity.response.OrganizationSearchList;
 import com.blossom.lineup.Organization.repository.OrganizationRepository;
+import com.blossom.lineup.base.Code;
+import com.blossom.lineup.base.exceptions.BusinessException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -16,6 +19,16 @@ import org.springframework.stereotype.Service;
 public class OrganizationServiceImpl implements OrganizationService{
 
     private final OrganizationRepository organizationRepository;
+
+    @Override
+    public OrganizationDto getOrganization(long organizationId) {
+        log.debug("주점 상세 정보 요청 : "+organizationId);
+
+        Organization organization = organizationRepository.findById(organizationId)
+                .orElseThrow(()-> new BusinessException(Code.ORGANIZATION_NOT_FOUND));
+
+        return OrganizationDto.of(organization);
+    }
 
     @Override
     public OrganizationSearchList searchOrganizations(String searchTerm, int pageNum) {
