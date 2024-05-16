de9a77ae81efb22cd74c13619a4420de06409a4c
Keon <lune0410k@naver.com>
2024-05-17 00:14:22 +0900

    feat: filter exception handler added

---
+++ src/main/java/com/blossom/lineup/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,76 @@
+package com.blossom.lineup.filter;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.blossom.lineup.base.Code;
+import com.blossom.lineup.base.Response;
+import com.blossom.lineup.base.exceptions.BusinessException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+	public static final ObjectMapper objectMapper = new ObjectMapper();
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+
+		try {
+			filterChain.doFilter(request, response);
+		} catch (BusinessException exception){
+
+			log.error(exception.getMessage());
+
+			response.setStatus(exception.getCode().getHttpStatus().value());
+			response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+			response.setCharacterEncoding("UTF-8");
+
+			setBusinessExceptionResponse(response, exception.getCode());
+
+		} catch (Exception exception){
+
+			log.error(exception.getMessage());
+
+			response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+			response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+			response.setCharacterEncoding("UTF-8");
+
+			setExceptionResponse(response, exception);
+
+		}
+
+	}
+
+	private void setBusinessExceptionResponse(HttpServletResponse response, Code code) {
+		try{
+			response.getWriter().write(objectMapper.writeValueAsString(Response.fail(code.getMessage(), code.getCode())));
+		} catch (IOException e){
+			log.error("check exception filter");
+			log.error(e.getMessage());
+		}
+
+	}
+
+	private void setExceptionResponse(HttpServletResponse response, Exception exception) {
+		try{
+			response.getWriter().write(objectMapper.writeValueAsString(Response.fail(Code.SERVER_FAIL.getCode(), exception.getMessage())));
+		} catch (IOException e){
+			log.error("check exception filter");
+			log.error(e.getMessage());
+		}
+
+	}
+
+}
