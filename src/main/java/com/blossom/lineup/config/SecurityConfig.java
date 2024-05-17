4b664759ff50de8c6e22a585b9be99b20f172f0a
je326 <mya326@naver.com>
2024-05-17 20:39:27 +0900

    feature: SecurityConfig 수정

--- src/main/java/com/blossom/lineup/config/SecurityConfig.java
+++ src/main/java/com/blossom/lineup/config/SecurityConfig.java
@@ -7,6 +7,9 @@ import com.blossom.lineup.filter.ExceptionHandlerFilter;
 import com.blossom.lineup.handler.LoginFailureHandler;
 import com.blossom.lineup.handler.LoginSuccessHandler;
 import com.blossom.lineup.jwt.filter.JwtAuthenticationFilter;
+import com.blossom.lineup.oauth2.handler.OAuth2LoginFailureHandler;
+import com.blossom.lineup.oauth2.handler.OAuth2LoginSuccessHandler;
+import com.blossom.lineup.oauth2.service.CustomOAuth2UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -37,6 +40,9 @@ public class SecurityConfig {
     private final CustomUserDetailsService customUserDetailsService;
     private final LoginSuccessHandler loginSuccessHandler;
     private final LoginFailureHandler loginFailureHandler;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
@@ -49,9 +55,13 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers("/","/css/**","/images/**", "/js/**",
                                 "/index.html", "/favicon.ico").permitAll() // 일반 경로 허용
-                        .requestMatchers("/api/sign-in/manager").permitAll() // 로그인 및 회원가입 경로 허용
+                        .requestMatchers("/api/sign-in/manager","/oauth2/authorization/kakao", "/api/oauth/sign-up", "/login/**").permitAll() // 로그인 및 회원가입 경로 허용
                         .requestMatchers("/api/manager/**").hasRole(Role.MANAGER.getRole()) // MANAGER 역할을 가진 사용자만 접근 가능
                         .anyRequest().authenticated()) // 그 외 모든 요청은 인증 요구
+                .oauth2Login((oauth2Login) ->
+                        oauth2Login.successHandler(oAuth2LoginSuccessHandler)
+                                .failureHandler(oAuth2LoginFailureHandler)
+                                .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService))))
                 .formLogin(AbstractHttpConfigurer::disable) // 폼 로그인 비활성화
                 .httpBasic(AbstractHttpConfigurer::disable) // 기본 HTTP 인증 비활성화
                 .addFilterAfter(CustomJsonAuthenticationFilter(), LogoutFilter.class)
