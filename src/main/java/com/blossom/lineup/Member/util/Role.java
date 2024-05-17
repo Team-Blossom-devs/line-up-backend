c26cbd4e6ea08f4c8e4d40b777748be2bf5de9f6
je326 <mya326@naver.com>
2024-05-17 20:33:28 +0900

    feature: OAuth 로그인 시 사용자 구별

--- src/main/java/com/blossom/lineup/Member/util/Role.java
+++ src/main/java/com/blossom/lineup/Member/util/Role.java
@@ -3,12 +3,12 @@ package com.blossom.lineup.Member.util;
 import lombok.Getter;
 
 /**
- * MANAGER: Organization 관리자, ADMIN: 프로젝트 소유자 관리자 계정(슈퍼 계정), USER: 이용객
+ * MANAGER: Organization 관리자, ADMIN: 프로젝트 소유자 관리자 계정(슈퍼 계정), USER: 이용객, GUEST: 소셜 로그인을 처음하는 USER
  */
 @Getter
 public enum Role {
 
-	MANAGER("MANAGER"), ADMIN("ADMIN"), USER("USER");
+	MANAGER("MANAGER"), ADMIN("ADMIN"), USER("USER"), GUEST("GUEST");
 
 	private final String role;
 
