310df1c5376b9fd75ed8c10a3b2a8a94511c7593
Keon <lune0410k@naver.com>
2024-05-19 16:16:09 +0900

    fix: fix conflict

--- src/main/java/com/blossom/lineup/Waiting/service/WaitingService.java
+++ src/main/java/com/blossom/lineup/Waiting/service/WaitingService.java
@@ -2,12 +2,15 @@ package com.blossom.lineup.Waiting.service;
 
 import com.blossom.lineup.Waiting.entity.request.WaitingRequest;
 import com.blossom.lineup.base.Response;
+import org.springframework.core.io.Resource;
 
 public interface WaitingService {
 
     void create(WaitingRequest request); // 대기 생성
 
-    Response<?> getWaitingStatus(Long organizationId);
+    Response<?> getWaitingStatus(Long organizationId); // 대기 현황 조회(waiting, pending)
+
+    Resource getQrCodeAsMultipartFile(Long waitingId); // pending 상태일 때, qr-code 가져오기
 
     void delete(Long waitingId); // 대기
 }
