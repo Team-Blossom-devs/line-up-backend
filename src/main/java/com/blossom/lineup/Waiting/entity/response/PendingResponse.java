cd6b3e2e260ce73016ac32def2ad4accd265cbac
HJunng <guswjd1gh@gmail.com>
2024-05-20 15:38:00 +0900

    refactor : 통일성을 위해 Long형으로 변경

--- src/main/java/com/blossom/lineup/Waiting/entity/response/PendingResponse.java
+++ src/main/java/com/blossom/lineup/Waiting/entity/response/PendingResponse.java
@@ -7,6 +7,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public class PendingResponse {
     String waitingStatus;
-    long remainMinutes; // 입장까지 남은 시간(분)
-    long waitingId;     // qr-code key를 위한 waitingId
+    Long waitingId;     // qr-code key를 위한 waitingId
+    Long remainMinutes; // 입장까지 남은 시간(분)
 }
