ef459fba77ef2b8fd78b5d32e8a87b35c656bf05
HJunng <guswjd1gh@gmail.com>
2024-05-20 15:40:24 +0900

    fix : 대기 id 포함해서 조회되도록 변경

--- src/main/java/com/blossom/lineup/Waiting/entity/response/WaitingResponse.java
+++ src/main/java/com/blossom/lineup/Waiting/entity/response/WaitingResponse.java
@@ -8,8 +8,9 @@ import lombok.Getter;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class WaitingResponse { // 대기 현황
-    private String waitingStatus; // 대기 상태
-    private int currentWaitingNumber; // 현재 내 순서
-    private int expectWaitingTime;  // 예상 대기 시간
-    private Integer headCount;            // 인원 수
+    private String waitingStatus;     // 대기 상태
+    private Long waitingId;           // 대기 id
+    private Integer currentWaitingNumber; // 현재 내 순서
+    private Integer expectWaitingTime;    // 예상 대기 시간
+    private Integer headCount;        // 인원 수
 }
