d6393ef4b3b951a1250f8108d62c8cf8048d07f1
HJunng <guswjd1gh@gmail.com>
2024-05-21 03:36:11 +0900

    feature : count 사용해서 기다리고 있는 팀 수 return

--- src/main/java/com/blossom/lineup/Waiting/repository/WaitingRepository.java
+++ src/main/java/com/blossom/lineup/Waiting/repository/WaitingRepository.java
@@ -16,18 +16,17 @@ import java.util.Optional;
 @Repository
 public interface WaitingRepository extends JpaRepository<Waiting, Long>, CustomWaitingRepository {
 
-    // 가장 최근의 Waiting 조회
-    Optional<Waiting> findFirstByEntranceStatusOrderByCreatedAtDesc(EntranceStatus status);
+    // 현재 기다리고 있는 팀이 몇 팀인지 조회
+    long countWaitingByEntranceStatus(EntranceStatus status);
+
+    // 내 대기id보다 먼저인 팀이 몇 팀인지 조회
+    @Query("SELECT COUNT(w) FROM Waiting w WHERE w.entranceStatus = 'WAITING' AND w.id < :waitingId")
+    long countWaitingBeforeMe(@Param("waitingId") Long waitingId);
 
     // 내가 이전에 organization에 대기를 걸어 놓은적이 있는지 확인.
-    List<Waiting> findByCustomerAndEntranceStatusIn(Customer customer, Collection<EntranceStatus> statuses);
+    Optional<Waiting> findByCustomerAndEntranceStatusIn(Customer customer, Collection<EntranceStatus> statuses);
 
-    // 나 이전의 대기 명단
-    // (organization에 대기하고 있고(waiting) 내 id보다 작은 id를 가지고 있는 waiting 명단을 id 오름차순으로 정렬)
-    @Query("SELECT w FROM Waiting w WHERE w.organization =:organization AND w.entranceStatus = 'WAITING' AND w.id = :id ORDER BY w.id")
-    List<Waiting> findBeforeMyWaiting(@Param("organization") Organization organization, @Param("id") Long id);
-
-    // 이용하고 있는 테이블의 모든 Waiting 정보 (입장시각순으로 정렬)
-    @Query("SELECT w FROM Waiting w WHERE w.organization =:organization AND w.entranceStatus = 'COMPLETE' ORDER BY w.entranceTime")
+    // 이용하고 있는 테이블의 모든 Waiting 정보 (updatedAt순으로 정렬)
+    @Query("SELECT w FROM Waiting w WHERE w.organization =:organization AND w.entranceStatus = 'COMPLETE' ORDER BY w.updatedAt DESC")
     List<Waiting> findUsingTables(@Param("organization") Organization organization);
 }
